eb53a508a1e02295101819fb022a1ea0b7993556
Tbernal121 <tomasbernalzuluaga@gmail.com>
2024-02-21 11:58:07 -0500

    :sparkles: se implementa el size dinámico para la traslacion y el escalado

--- ImageOperations.cpp
+++ ImageOperations.cpp
@@ -18,14 +18,20 @@ void matrixMultiply_Secuencial(const vector<vector<double>>& A, const vector<vec
 }
 
 void traslacion(const vector<vector<Pixel>>& imagen, vector<vector<Pixel>>& resultado, double dx, double dy) {
+    int ancho = imagen[0].size();
+    int alto = imagen.size();
+
+    // Redimensionar la matriz resultado
+    resultado.resize(alto+dy, vector<Pixel>(ancho+dx));
+
     vector<vector<double>> T = {
         {1, 0, dx},
         {0, 1, dy},
         {0, 0, 1}
     };
 
-    for (double i = 0; i < imagen.size(); ++i) {
-        for (double j = 0; j < imagen[0].size(); ++j) {
+    for (double i = 0; i < (alto+dy); ++i) {
+        for (double j = 0; j < (ancho+dx) ; ++j) {
             vector<vector<double>> punto = {
                 {j},
                 {i},
@@ -38,7 +44,7 @@ void traslacion(const vector<vector<Pixel>>& imagen, vector<vector<Pixel>>& resu
             double x = puntoTrasladado[0][0];
             double y = puntoTrasladado[1][0];
 
-            if (x >= 0 && x < resultado[0].size() && y >= 0 && y < resultado.size()) {
+            if (x >= 0 && x < (ancho+dx) && y >= 0 && y < (alto+dy)) {
                 resultado[y][x] = imagen[i][j];
             }
         }
@@ -49,7 +55,7 @@ void escalado(const vector<vector<Pixel>>& imagen, vector<vector<Pixel>>& result
     int ancho = imagen[0].size();
     int alto = imagen.size();
 
-    // Crear una nueva imagen con el tamaño escalado
+    // Redimensionar la matriz resultado
     resultado.resize(alto * sy, vector<Pixel>(ancho * sx));
 
     // Escalar la imagen
@@ -69,17 +75,28 @@ void escalado(const vector<vector<Pixel>>& imagen, vector<vector<Pixel>>& result
 }
 
 void rotacionOrigen(const vector<vector<Pixel>> &imagen, vector<vector<Pixel>> &resultado, double angulo){
+    int ancho = imagen[0].size();
+    int alto = imagen.size();
+
+    // Calcular el tamaño del marco después de la rotación
+    double radianes = angulo * 3.1416 / 180;
+    double diagonal = sqrt(ancho * ancho + alto * alto);
+    int nuevoAncho = abs(diagonal * cos(radianes));
+    int nuevoAlto = abs(diagonal * sin(radianes));
+
+    // Redimensionar la matriz resultado para que tenga el nuevo tamaño
+    resultado.resize(nuevoAlto, vector<Pixel>(nuevoAncho, Pixel())); // Pixel() es un pixel vacío
+
     int dy = resultado.size()/2;
     int dx = resultado[0].size()/2;
-    double radianes = angulo * 3.1416 / 180;
     vector<vector<double>> R = {
         {cos(radianes), sin(radianes), 0},
         {-sin(radianes), cos(radianes), 0},
         {0, 0, 1}
     };
 
-    for (double i = 0; i < imagen.size(); ++i) {
-        for (double j = 0; j < imagen[0].size(); ++j) {
+    for (double i = 0; i < alto; ++i) {
+        for (double j = 0; j < ancho; ++j) {
             vector<vector<double>> punto = {
                 {j},
                 {i},
@@ -89,11 +106,14 @@ void rotacionOrigen(const vector<vector<Pixel>> &imagen, vector<vector<Pixel>> &
             matrixMultiply_Secuencial(R, punto, puntoRotado);
             int x = puntoRotado[0][0];
             int y = puntoRotado[1][0];
-            resultado[y+dy][x+dx] = imagen[i][j];
+            if (y+dy >= 0 && y+dy < nuevoAlto && x+dx >= 0 && x+dx < nuevoAncho) {
+                resultado[y+dy][x+dx] = imagen[i][j];
+            }
         }
     }
-    
 }
+
+
 
 void cizallamientoX (const vector<vector<Pixel>> &imagen, vector<vector<Pixel>> &resultado, double shx){
     double radianes = shx * 3.1416 / 180;
